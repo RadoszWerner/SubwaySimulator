37039de8cdc587e59128bca8b84d3c72dccae6e0
Hendrykik <hendrykik@wp.pl>
2025-06-11 16:59:39 +0200

    implementation

--- Assets/Scripts/PlayerLogic/PlayerController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField] private float moveSpeed = 1f;
-    
-}
